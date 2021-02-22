69517150036d89091770edee811453b958b306a9
FranRom <maildefranz@gmail.com>
2021-02-22 23:23:04 +0100

    Fix eslint warning + PUBLIC_URL env

--- src/components/HeaderLinks/HeaderLinks.js
+++ src/components/HeaderLinks/HeaderLinks.js
@@ -1,21 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
 import { renderIcon } from '@download/blockies';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Avatar from '@material-ui/core/Avatar';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import { WbSunny, NightsStay } from '@material-ui/icons';
-import CustomDropdown from 'components/CustomDropdown/CustomDropdown.js';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { makeStyles } from '@material-ui/core/styles';
+import { NightsStay, WbSunny } from '@material-ui/icons';
 import Button from 'components/CustomButtons/Button.js';
+import CustomDropdown from 'components/CustomDropdown/CustomDropdown.js';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import styles from './styles';
 
 const useStyles = makeStyles(styles);
 
-const HeaderLinks = ({ connected, address, connectWallet, disconnectWallet, isNightMode, setNightMode }) => {
+const HeaderLinks = ({
+  connected,
+  address,
+  connectWallet,
+  disconnectWallet,
+  isNightMode,
+  setNightMode,
+}) => {
   const classes = useStyles();
   const { t, i18n } = useTranslation();
   const [lng, setLanguage] = useState('en');
@@ -24,14 +31,14 @@ const HeaderLinks = ({ connected, address, connectWallet, disconnectWallet, isNi
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const cachedLanguage = i18n.language
+    const cachedLanguage = i18n.language;
     if (!cachedLanguage) {
       return;
     }
 
-    const languageCode = cachedLanguage.split('-')[0].toLowerCase()
-    setLanguage(languageCode)
-  }, [])
+    const languageCode = cachedLanguage.split('-')[0].toLowerCase();
+    setLanguage(languageCode);
+  }, [i18n.language]);
 
   useEffect(() => {
     if (!connected) {
