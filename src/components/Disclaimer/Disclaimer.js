c2c8ad49777acee9063666fb29821c19b32e2987
FranRom <maildefranz@gmail.com>
2021-03-09 22:45:27 +0100

    Add text as prop in Disclaimer

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -2,17 +2,13 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React, { memo } from 'react';
-import { useTranslation } from 'react-i18next';
 
 import styles from './styles';
 
 const useStyles = makeStyles(styles);
 
-const Disclaimer = ({ isConstructionMode }) => {
-  const { t } = useTranslation();
+const Disclaimer = ({ isConstructionMode, text }) => {
   const classes = useStyles();
-
-  const text = isConstructionMode ? t('disclaimer-construction') : t('Disclaimer');
   const disclaimerStyles = isConstructionMode ? classes.constructionMode : classes.disclaimer;
 
   return (
