508d65f1a2100295d3f128a9b6f72c6ac834a0fb
Fran Rom <32134460+FranRom@users.noreply.github.com>
2021-03-09 18:56:14 +0100

    Revert "[Feature] Remove construction disclaimer"

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -1,9 +1,12 @@
 import Pools from 'features/vault/components/Pools/Pools';
 import React from 'react';
 
+import Disclaimer from '../../components/Disclaimer/Disclaimer';
+
 export default function HomePage() {
   return (
     <>
+      <Disclaimer isConstructionMode />
       <Pools fromPage="home" />
     </>
   );
